ccc07848e4a92a1917bb7c2ced4fe85314c719de
LucTran-git <114346146+LucTran-git@users.noreply.github.com>
2023-04-09 20:58:36 -0700

    Implemented IAM policy check

--- service_analyzers/iam.py
+++ service_analyzers/iam.py
@@ -3,22 +3,75 @@ import json
 import datetime
 import botocore
 import os
+import re
 
 from .abstract import * 
 # Note the use of * in importing; this way,
 # abstract functions are called as iam.func_name()
 # instead of iam.abstract.func_name() 
 
-def analyze_policies(iam):
-    '''This is a docstring. Use this to give an explanation of your method when you hover over it in the IDE
+policy_docs = {}
+policy_warnings = {} # dict of (list of dicts) 
+            # { 'policy_name' : [{'warning' : 'text', 'explanation' : 'text', 'recommendation' : 'text'}, ...] }
+            # We separate the warning info in this way to make it easier to format the report in different ways e.g. txt vs csv
+
+passrole_actions = ['*','iam:*','iam:PassRole']
+
+def check_policy_passrole_overly_permissive(policy, policy_doc):
+    """Check that policy does not allow PassRole to pass roles for any service.
+    If it did, an IAM identity with this policy could grant extra permissions
+    to any service, posing major security risk"""
+    for stmt in policy_doc['Statement']:
+        if stmt['Effect'] != 'Allow':
+            continue
+
+        passrole_overly_permissive = False
+        for action in stmt['Action']:
+            if action in passrole_actions:
+                for resource in stmt['Resource']:
+                    if resource == '*':
+                        passrole_overly_permissive = True
+                        break
+        if passrole_overly_permissive == False:
+            continue
+        
+        try:
+            policy_warnings[policy['PolicyName']]
+        except KeyError:
+            policy_warnings[policy['PolicyName']] = []
+
+        policy_warnings[policy['PolicyName']].append(
+                    {
+"Warning" : """
+    iam:PassRole overly permissive
+""",
+"Explanation" : """
+    Any IAM identity (i.e. users, groups, or roles) 
+    with this policy can assign roles to an instance
+    of any resource.
+
+    As such, this policy can be exploited to grant
+    unintended permissions to any resource.
     
-    You can make a docstring by putting a multi-line comment immediately below a function def
+    More info: https://docs.aws.amazon.com/IAM/latest/UserGuide/id_roles_use_passrole.html
+""",
+"Recommendation": """
+    If you want iam:PassRole to be permitted:
+    - Specify resource ARNs explicitly in 'Resource'
+    - Add the iam:PassedToService condition key to the statement
     
-    Also works for files when importing them, like I did with abstract.py'''
-    return NotImplementedError
+    If you do not want iam:PassRole to be permitted:
+    - Specify iam actions explicitly in 'Action'
+"""
+                    }
+        )
+
+def analyze_local_managed_policies(iam):
+    '''Get and analyze policies'''
+    print('Analyzing local managed policies...')
     response = iam.list_policies(
-        #Scope='All'|'AWS'|'Local',
-        #OnlyAttached=True,  # Only check policies in use, b/c those are the relevant ones
+        Scope= 'Local', #'All'|'AWS'|'Local',
+        OnlyAttached=True,  # Only check policies in use, b/c those are the relevant ones
                             # Could use False for more thorough check, in case customer might change policies 
         #PathPrefix='string',
         #PolicyUsageFilter='PermissionsPolicy'|'PermissionsBoundary',
@@ -26,14 +79,44 @@ def analyze_policies(iam):
                             # response to continue where you left off
         #MaxItems=123
     )
+
+    # Obtain policy info, and perform some single-policy checks while we're at it
     for policy in response['Policies']:
-        print(policy)
+        response = iam.get_policy_version(
+        PolicyArn=policy['Arn'],
+        VersionId=policy['DefaultVersionId']
+        )
 
-def write_analysis_to_file():
-    '''Write errors (and maybe other data), and recommendations to a log file'''
-    return NotImplementedError
+        # Get policy doc (i.e. JSON file) of this specific policy
+        policy_doc = response['PolicyVersion']['Document']
+        # Add the policy doc to policy_docs dict
+        policy_docs[policy['PolicyName']] = policy_doc
 
-#def run_all_checks(iam):
+        # Do the checks we can do on single policies
+        check_policy_passrole_overly_permissive(policy, policy_doc)
+        filepath = write_analysis_to_file('txt')
+    print(f'Analysis finished and written to {filepath}')
+
+def write_analysis_to_file(file_format):
+    '''Write warnings and recommendations to a log file\n
+    Parameters: file format\n
+    Returns: filepath of written file'''
+    
+    filepath = 'logs/iam_report.txt'
+    if file_format == 'txt':
+        with open(filepath, 'w') as file:
+            for policy_name, warnings in policy_warnings.items():
+                file.write(
+f"""----- Policy Name: {policy_name} -----
+{json.dumps(policy_docs[policy_name], indent=4)}
+                        
+""")
+                for w in warnings:
+                    for label, text in w.items():
+                        file.write("| " + label + " |" + text)
+    return filepath
+
+def run_all_checks(iam):
     print('Running all checks for iam...')
     return NotImplementedError
 
--- test.py
+++ test.py
@@ -1,21 +1,33 @@
 import boto3
+import json
 
 from service_analyzers import iam
 from service_analyzers import ec2
 from service_analyzers import s3
 from service_analyzers import vpc
 
-# accessKeyId = ''
-# secretAccessKey = ''
-# session = boto3.Session(aws_access_key_id=accessKeyId, aws_secret_access_key=secretAccessKey)
+with open('Credentials.json', 'r') as credsFile:
+    credsData = json.load(credsFile)
+
+    if (not credsData['AWS']['access_key_id'] or not credsData['AWS']['secret_access_key']):
+        credsData['AWS']['access_key_id'] = input("Please enter Access Key ID:")
+        credsData['AWS']['secret_access_key'] = input("Please enter Secret Access Key:")
+
+        with open('Credentials.json', 'w') as credsFileOut:    
+            json.dump(credsData, credsFileOut, indent=4)
+
+accessKeyId = credsData['AWS']['access_key_id']
+secretAccessKey = credsData['AWS']['secret_access_key']
+
+session = boto3.Session(aws_access_key_id=accessKeyId, aws_secret_access_key=secretAccessKey)
 
 clients = {}
-clients['iam'] = boto3.client('iam')
+clients['iam'] = session.client('iam')
 # clients['ec2'] = session.client('ec2')
 # clients['s3'] = session.client('s3')
 # clients['vpc'] = session.client('vpc')
 
-iam.run_all_checks(clients['iam'])
-iam.analyze_policies(clients['iam'])
+#iam.run_all_checks(clients['iam'])
+iam.analyze_local_managed_policies(clients['iam'])
 
-print(clients['iam'])
+#print(clients['iam'])
